a922fae85d278eff5883f1646d5e314ccd9b7a93
Michael Sunarlim <michael@sunarlim.com>
2015-11-03 19:49:19 +0700

    Removed [[ ]] to make it easier for users to enter API and list ID

--- subscribe.php
+++ subscribe.php
@@ -1,12 +1,12 @@
-<?php	
+<?php
 	require_once 'inc/MCAPI.class.php';
-	$api = new MCAPI('[[YOUR_API_KEY]]');	
+	$api = new MCAPI('ENTER_YOUR_API_KEY_HERE');
 	$merge_vars = array('FNAME'=>$_POST["fname"], 'LNAME'=>$_POST["lname"]);
-	
+
 	// Submit subscriber data to MailChimp
 	// For parameters doc, refer to: http://apidocs.mailchimp.com/api/1.3/listsubscribe.func.php
-	$retval = $api->listSubscribe( '[[YOUR_LIST_ID]]', $_POST["email"], $merge_vars, 'html', false, true );
-	
+	$retval = $api->listSubscribe( 'ENTER_YOUR_LIST_ID_HERE', $_POST["email"], $merge_vars, 'html', false, true );
+
 	if ($api->errorCode){
 		echo "<h4>Please try again.</h4>";
 	} else {
